094e25d2330bb94ca20ebc121faa02a1fd383875
@Mamad118 <mamadalisilavi@gmail.com>
2024-08-25 21:57:20 +0330

    test

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const { message } = require('telegraf/filters')
 
 const bot = new Telegraf('7128024687:AAGSROlHxA1F5eshJNWwm_2N1n4VaPueS28')
 bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-// bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
-// bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.on(message("document"),(msg)=>msg.forwardMessage(1312290405,msg.update.message.message_id))    
+bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
+bot.on(message("document"),(msg)=>{
+    msg.forwardMessage(1312290405,msg.update.message.message_id)
+    msg.sendMessage()
+})    
 bot.launch()
 // msg.forwardMessage(1312290405)
 
